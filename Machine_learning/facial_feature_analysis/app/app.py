5691178d969e4ee1301670f843624fc63d803d7a
mzaoualim <74496123+mzaoualim@users.noreply.github.com>
2025-04-01 15:21:28 +0000

    s

--- Machine_learning/facial_feature_analysis/app/app.py
+++ Machine_learning/facial_feature_analysis/app/app.py
@@ -130,11 +130,11 @@ def main():
             in_range = False
             for strength, band in range_dict.items():
                 if band[0] <= calculated_jaw_ratio <= band[1]:
-                    st.write(strength)
+                    st.write(f' The computed ratio equals : {calculated_jaw_ratio}\n.The captured jaws are {strength}')
                     in_range = True
             
             if not in_range:
-                st.write('Unable to determine the Jaw Strength \n The computed Jaw ratio is located outside the domaine knowledge range\n')
+                st.write('Unable to determine the Jaw Strength\nThe computed Jaw Strength ratio is located outside the domaine knowledge range\n')
 
             # st.write(np.round(calculated_jaw_ratio, 2), classify_jaw_strength(calculated_jaw_ratio))
 
