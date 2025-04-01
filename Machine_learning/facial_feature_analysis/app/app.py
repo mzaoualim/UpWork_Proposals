1215fa920e6dd2dd6d3669613051e5088f6670ab
mzaoualim <74496123+mzaoualim@users.noreply.github.com>
2025-04-01 14:27:49 +0000

    k

--- Machine_learning/facial_feature_analysis/app/app.py
+++ Machine_learning/facial_feature_analysis/app/app.py
@@ -129,7 +129,7 @@ def main():
             st.write(range_dict.items())
 
             for strength, band in range_dict.items():
-                if list(band)[0] <= jaw_ratio <= list(band)[1]:
+                if list(band)[0] <= calculated_jaw_ratio <= list(band)[1]:
                     st.write(strength)
 
             st.write(np.round(calculated_jaw_ratio, 2), classify_jaw_strength(calculated_jaw_ratio))
