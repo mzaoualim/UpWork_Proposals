07a7345c0089d6fa4fdaec6042e008b2d7df7106
mzaoualim <74496123+mzaoualim@users.noreply.github.com>
2025-10-31 18:39:32 +0100

    Delete Machine_learning/XAU-USD directory

--- Machine_learning/XAU-USD/utils/vwap.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def vwap(df: pd.DataFrame, price_col: str = 'close', vol_col: str = 'volume', window: int = 20) -> pd.Series:
-    tp = df[price_col]
-    pv = tp * df[vol_col]
-    rolling_pv = pv.rolling(window=window, min_periods=1).sum()
-    rolling_v = df[vol_col].rolling(window=window, min_periods=1).sum()
-    return rolling_pv / rolling_v
